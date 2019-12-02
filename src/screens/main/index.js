f0fd645923bb8b38683e4f2d98bb44f328903a9d
Ezequiel <ezequiel1de1oliveira@gmail.com>
2019-12-02 10:34:17 -0300

    Improving design and add form for insertion
    Signed-off-by: Ezequiel <ezequiel1de1oliveira@gmail.com>

--- src/screens/main/index.js
+++ src/screens/main/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import Skill from './../../components/skill';
 
 class Main extends React.Component {
-    state = {
-        unlocked: [],
-        skills: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            unlocked: [],
+            skills: [],
+            name: '',
+            img: '',
+            depends: '',
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleImg = this.handleImg.bind(this);
+        this.handleDepends = this.handleDepends.bind(this);
+
     }
 
     componentDidMount() {
@@ -42,6 +54,19 @@ class Main extends React.Component {
                 name: 'FireThunder',
                 img: 'https://cdna.artstation.com/p/assets/images/images/000/075/234/large/fire_and_thunder_by_amorphisss-d205b1a.jpg?1400771486',
                 dependsOn: ['Thunder', 'FireBreath'],
+            }, {
+                name: 'Arcane Bolt',
+                img: 'https://gamepedia.cursecdn.com/minionmasters_gamepedia_en/thumb/b/bf/ArcanePrison.jpg/216px-ArcanePrison.jpg?version=65f7f7c9a48bd3f5811947916c85fbf7',
+                dependsOn: ['FireThunder'],
+            }, {
+                name: 'Hypnotize',
+                img: 'https://gamepedia.cursecdn.com/minionmasters_gamepedia_en/a/a8/Hypnotize.jpg?version=d74ef2ac9fff7b58c60a6920879b214e',
+                dependsOn: ['FireBreath'],
+            },
+            {
+                name: 'Infiltration',
+                img: 'https://gamepedia.cursecdn.com/minionmasters_gamepedia_en/thumb/6/63/Infiltrators.jpg/216px-Infiltrators.jpg?version=093b92c1edd70ce7c6a0ba1de70ab001',
+                dependsOn: ['Hypnotize','FireBreath','FireBall','Flames'],
             },
         ]
 
@@ -53,7 +78,7 @@ class Main extends React.Component {
     }
 
     topologicOrder(objs) {
-        objs = objs.map(obj => {return {...obj, tempDependsOn: obj.dependsOn}})
+        objs = objs.map(obj => { return { ...obj, tempDependsOn: obj.dependsOn } })
         let topologic = [];
 
         while (objs.length > 0) {
@@ -62,13 +87,13 @@ class Main extends React.Component {
             });
 
             if (ready.length === 0) return undefined;
-            
+
             const notReady = objs.filter(obj => obj.dependsOn.length > 0);
 
             ready.map(readyObj => {
                 topologic.push(readyObj);
 
-                objs = notReady.map( notReadyObj => {
+                objs = notReady.map(notReadyObj => {
                     notReadyObj.dependsOn = notReadyObj.dependsOn.filter(
                         dependency => dependency !== readyObj.name
                     )
@@ -77,11 +102,13 @@ class Main extends React.Component {
             })
         }
 
-        return topologic.map(obj => {return {
-            name: obj.name,
-            img: obj.img,
-            dependsOn: obj.tempDependsOn
-        }});
+        return topologic.map(obj => {
+            return {
+                name: obj.name,
+                img: obj.img,
+                dependsOn: obj.tempDependsOn
+            }
+        });
     }
 
     unlock(name) {
@@ -97,8 +124,6 @@ class Main extends React.Component {
 
     checkStatus(obj) {
 
-        console.log(obj)
-
         if (this.state.unlocked.includes(obj.name)) {
             return 'unlocked';
         } else if (obj.dependsOn.length === 0) {
@@ -111,6 +136,36 @@ class Main extends React.Component {
         return 'locked';
     }
 
+    handleName(e) {
+        this.setState({ name: e.target.value })
+    }
+
+    handleImg(e) {
+        this.setState({ img: e.target.value })
+    }
+
+    handleDepends(e) {
+        this.setState({ depends: e.target.value })
+    }
+
+    async handleSubmit(e) {
+        let skill = await {
+            "name": this.state.name,
+            "img": this.state.img,
+            "dependsOn": this.state.depends.split(',')
+        }
+
+        let skills = this.state.skills;
+
+        skills.push(skill);
+
+        var ordered = this.topologicOrder(skills);
+
+        this.setState({
+            skills: ordered
+        })
+    }
+
     render() {
 
         if (this.state.skills === undefined) {
@@ -120,18 +175,42 @@ class Main extends React.Component {
         }
 
         return (
-            <div>
-                {this.state.skills.map((skill, i) =>
-                    <Skill
-                        key={`skill${i}`}
-                        skill={skill}
-                        unlock={() => this.unlock(skill.name)}
-                        status={this.checkStatus(skill)}
+            <div style={{ display: 'flex',marginLeft: "100px" }}>
+                <h1>Skills:</h1>
+                <div style={{ display: 'flex', alignItems: 'start', justifyContent: 'center', flexDirection: "column", marginLeft: '100px' }}>
+                    {this.state.skills.map((skill, i) =>
+                        <Skill
+                            key={`skill${i}`}
+                            skill={skill}
+                            unlock={() => this.unlock(skill.name)}
+                            status={this.checkStatus(skill)}
+                        />
+                    )}
+                </div>
+                <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', margin: '250px'}}>
+                    <textarea
+                        type="text"
+                        rows={1}
+                        value={this.state.skill}
+                        onChange={this.handleName}
                     />
-                )}
+                    <textarea
+                        type="text"
+                        rows={1}
+                        value={this.state.skill}
+                        onChange={this.handleImg}
+                    />
+                    <textarea
+                        type="text"
+                        rows={3}
+                        value={this.state.skill}
+                        onChange={this.handleDepends}
+                    />
+                    <button onClick={this.handleSubmit} >Ok</button>
+                </div>
             </div>
         );
     }
 }
- 
+
 export default Main;
